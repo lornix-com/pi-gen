0a5da5e2ecd7b7f7c9271bff417afb1d761860d1
L Nix <lornix@lornix.com>
2020-05-29 11:48:12 -0700

    Oops, fix trying to rm non-existant file, add empty string protection to compare

--- stage2/04-tweak-sudoers/02-run.sh
+++ stage2/04-tweak-sudoers/02-run.sh
@@ -2,7 +2,7 @@
 
 # make proper adjustment to /etc/sudoers.d/010_pi-nopasswd file
 FILEPATH="${ROOTFS_DIR}/etc/sudoers.d"
-if [ "${FIRST_USER_NAME}" != "pi" ]; then 
+if [ "x${FIRST_USER_NAME}" != "xpi" ]; then
     echo "${FIRST_USER_NAME} ALL=(ALL) NOPASSWD: ALL" > "${FILEPATH}/010_${FIRST_USER_NAME}-nopasswd"
-    rm "${FILEPATH}/010_pi-nopasswd"
+    rm -f "${FILEPATH}/010_pi-nopasswd"
 fi
